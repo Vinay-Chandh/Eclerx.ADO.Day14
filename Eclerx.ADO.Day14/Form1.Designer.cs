ec182222f775153a823eb3a3e982dadf670e23a7
Kallem.Reddy <Kallem.Reddy@ECXLC5499>
2022-07-06 01:56:20 +0530

    Add project files.

---
+++ Eclerx.ADO.Day14/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace EClerx.ADO.Day14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblEmployeeCode = new System.Windows.Forms.Label();
+            this.LblFirstName = new System.Windows.Forms.Label();
+            this.LblCity = new System.Windows.Forms.Label();
+            this.LblState = new System.Windows.Forms.Label();
+            this.TxtEmpCode = new System.Windows.Forms.TextBox();
+            this.TxtFirstName = new System.Windows.Forms.TextBox();
+            this.TxtCity = new System.Windows.Forms.TextBox();
+            this.TxtState = new System.Windows.Forms.TextBox();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.BtnAddNew = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.LblSocialSecurityNumber = new System.Windows.Forms.Label();
+            this.TxtSocialSecurityNo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblEmployeeCode
+            // 
+            this.LblEmployeeCode.AutoSize = true;
+            this.LblEmployeeCode.Location = new System.Drawing.Point(134, 39);
+            this.LblEmployeeCode.Name = "LblEmployeeCode";
+            this.LblEmployeeCode.Size = new System.Drawing.Size(81, 13);
+            this.LblEmployeeCode.TabIndex = 0;
+            this.LblEmployeeCode.Text = "Employee Code";
+            // 
+            // LblFirstName
+            // 
+            this.LblFirstName.AutoSize = true;
+            this.LblFirstName.Location = new System.Drawing.Point(134, 79);
+            this.LblFirstName.Name = "LblFirstName";
+            this.LblFirstName.Size = new System.Drawing.Size(60, 13);
+            this.LblFirstName.TabIndex = 1;
+            this.LblFirstName.Text = "First Name ";
+            // 
+            // LblCity
+            // 
+            this.LblCity.AutoSize = true;
+            this.LblCity.Location = new System.Drawing.Point(134, 121);
+            this.LblCity.Name = "LblCity";
+            this.LblCity.Size = new System.Drawing.Size(24, 13);
+            this.LblCity.TabIndex = 2;
+            this.LblCity.Text = "City";
+            // 
+            // LblState
+            // 
+            this.LblState.AutoSize = true;
+            this.LblState.Location = new System.Drawing.Point(134, 161);
+            this.LblState.Name = "LblState";
+            this.LblState.Size = new System.Drawing.Size(32, 13);
+            this.LblState.TabIndex = 3;
+            this.LblState.Text = "State";
+            // 
+            // TxtEmpCode
+            // 
+            this.TxtEmpCode.Location = new System.Drawing.Point(283, 36);
+            this.TxtEmpCode.Name = "TxtEmpCode";
+            this.TxtEmpCode.Size = new System.Drawing.Size(191, 20);
+            this.TxtEmpCode.TabIndex = 4;
+            // 
+            // TxtFirstName
+            // 
+            this.TxtFirstName.Location = new System.Drawing.Point(283, 72);
+            this.TxtFirstName.Name = "TxtFirstName";
+            this.TxtFirstName.Size = new System.Drawing.Size(191, 20);
+            this.TxtFirstName.TabIndex = 5;
+            // 
+            // TxtCity
+            // 
+            this.TxtCity.Location = new System.Drawing.Point(283, 118);
+            this.TxtCity.Name = "TxtCity";
+            this.TxtCity.Size = new System.Drawing.Size(191, 20);
+            this.TxtCity.TabIndex = 6;
+            // 
+            // TxtState
+            // 
+            this.TxtState.Location = new System.Drawing.Point(283, 158);
+            this.TxtState.Name = "TxtState";
+            this.TxtState.Size = new System.Drawing.Size(191, 20);
+            this.TxtState.TabIndex = 7;
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Location = new System.Drawing.Point(501, 33);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(75, 23);
+            this.BtnSearch.TabIndex = 8;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            this.BtnSearch.Click += new System.EventHandler(this.BtnSearch_Click_1);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(237, 254);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 9;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click_1);
+            // 
+            // BtnAddNew
+            // 
+            this.BtnAddNew.Location = new System.Drawing.Point(318, 254);
+            this.BtnAddNew.Name = "BtnAddNew";
+            this.BtnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.BtnAddNew.TabIndex = 10;
+            this.BtnAddNew.Text = "Add New";
+            this.BtnAddNew.UseVisualStyleBackColor = true;
+            this.BtnAddNew.Click += new System.EventHandler(this.BtnAddNew_Click_1);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(399, 254);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.BtnUpdate.TabIndex = 11;
+            this.BtnUpdate.Text = "Update";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click_1);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(501, 254);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 12;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click_1);
+            // 
+            // LblSocialSecurityNumber
+            // 
+            this.LblSocialSecurityNumber.AutoSize = true;
+            this.LblSocialSecurityNumber.Location = new System.Drawing.Point(134, 202);
+            this.LblSocialSecurityNumber.Name = "LblSocialSecurityNumber";
+            this.LblSocialSecurityNumber.Size = new System.Drawing.Size(117, 13);
+            this.LblSocialSecurityNumber.TabIndex = 13;
+            this.LblSocialSecurityNumber.Text = "Social Security Number";
+            // 
+            // TxtSocialSecurityNo
+            // 
+            this.TxtSocialSecurityNo.Location = new System.Drawing.Point(283, 199);
+            this.TxtSocialSecurityNo.Name = "TxtSocialSecurityNo";
+            this.TxtSocialSecurityNo.Size = new System.Drawing.Size(191, 20);
+            this.TxtSocialSecurityNo.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtSocialSecurityNo);
+            this.Controls.Add(this.LblSocialSecurityNumber);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnAddNew);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnSearch);
+            this.Controls.Add(this.TxtState);
+            this.Controls.Add(this.TxtCity);
+            this.Controls.Add(this.TxtFirstName);
+            this.Controls.Add(this.TxtEmpCode);
+            this.Controls.Add(this.LblState);
+            this.Controls.Add(this.LblCity);
+            this.Controls.Add(this.LblFirstName);
+            this.Controls.Add(this.LblEmployeeCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblEmployeeCode;
+        private System.Windows.Forms.Label LblFirstName;
+        private System.Windows.Forms.Label LblCity;
+        private System.Windows.Forms.Label LblState;
+        private System.Windows.Forms.TextBox TxtEmpCode;
+        private System.Windows.Forms.TextBox TxtFirstName;
+        private System.Windows.Forms.TextBox TxtCity;
+        private System.Windows.Forms.TextBox TxtState;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button BtnAddNew;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Label LblSocialSecurityNumber;
+        private System.Windows.Forms.TextBox TxtSocialSecurityNo;
+    }
+}
+
---
+++ Eclerx.ADO.Day14/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EClerx.ADO.Day14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection con = null;
+        private SqlCommand cmd = null;
+        private SqlDataReader reader = null;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //Create Sql Connection
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRConn"].ConnectionString);
+
+            //Create Sql Command - Select Query or DML Query or Stored Procedure
+            cmd = new SqlCommand("Select cEmployeeCode, vFirstName, cCity, cState from Employee", con);
+
+            //Open the connection
+            con.Open();
+
+            //Execute the command and fetch datas
+            reader = cmd.ExecuteReader();
+
+            //Read the first row
+            reader.Read();
+
+            //Store into releavant TextBoxes
+            TxtEmpCode.Text = reader["cEmployeeCode"].ToString();
+            TxtFirstName.Text = reader["vFirstName"].ToString();
+            TxtCity.Text = reader["cCity"].ToString();
+            TxtState.Text = reader["cState"].ToString();
+
+            //Close the reader object
+            reader.Close();
+
+            //Dispose Command Object
+            cmd.Dispose();
+
+            //Close the connection
+            con.Close();
+        }
+
+        private void BtnReset_Click_1(object sender, EventArgs e)
+        {
+            this.ClearText();
+        }
+        public void ClearText()
+        {
+            TxtEmpCode.Text = "";
+            TxtFirstName.Clear();
+            TxtCity.Text = "";
+            TxtState.Text = "";
+            TxtSocialSecurityNo.Text = "";
+            TxtEmpCode.Focus();
+        }
+
+        private void BtnSearch_Click_1(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRConn"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("Select vFirstName, cCity, cState, cSocialSecurityNo from Employee Where cEmployeeCode=@EmployeeCode", con))
+                {
+                    cmd.Parameters.AddWithValue("@EmployeeCode", TxtEmpCode.Text);
+
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    using (reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            TxtFirstName.Text = reader["vFirstName"].ToString();
+                            TxtCity.Text = reader["cCity"].ToString();
+                            TxtState.Text = reader["cState"].ToString();
+                            TxtSocialSecurityNo.Text = reader["cSocialSecurityNo"].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No Record");
+                            this.ClearText();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnAddNew_Click_1(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRConn"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("usp_AddNewEmployee", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmployeeCode", TxtEmpCode.Text);
+                    cmd.Parameters.AddWithValue("@FirstName", TxtFirstName.Text);
+                    cmd.Parameters.AddWithValue("@City", TxtCity.Text);
+                    cmd.Parameters.AddWithValue("@State", TxtState.Text);
+                    cmd.Parameters.AddWithValue("@SocialSecurityNo", TxtSocialSecurityNo.Text);
+
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    cmd.ExecuteNonQuery(); //used for DML operation
+                }
+            }
+            MessageBox.Show("New Employee Created");
+        }
+
+        private void BtnUpdate_Click_1(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRConn"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("usp_UpdateCityAndStateByCode", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@City", TxtCity.Text);
+                    cmd.Parameters.AddWithValue("@State", TxtState.Text);
+                    cmd.Parameters.AddWithValue("@EmployeeCode", TxtEmpCode.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Record updated successfully");
+        }
+
+        private void BtnDelete_Click_1(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRConn"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("usp_DeleteEmlopyee", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmployeeCode", TxtEmpCode.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Record deleted successfully");
+        }
+    }
+}
